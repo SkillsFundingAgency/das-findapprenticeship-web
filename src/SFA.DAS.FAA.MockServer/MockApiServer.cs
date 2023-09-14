e90e873febbec949253873fb5ce32bea790c8487
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-09-14 22:29:29 +0100

    FAI-911 - Add mock server

---
+++ src/SFA.DAS.FAA.MockServer/MockApiServer.cs
@@ -0,0 +1,38 @@
+using System.Text.RegularExpressions;
+using WireMock.Logging;
+using WireMock.Net.StandAlone;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+using WireMock.Types;
+
+namespace SFA.DAS.FAA.MockServer;
+
+public static class MockApiServer
+{
+    public static IWireMockServer Start()
+    {
+        var settings = new WireMockServerSettings
+        {
+            Port = 5003,
+            Logger = new WireMockConsoleLogger()
+        };
+        
+        var server = StandAloneApp.Start(settings);
+        
+        server.Given(Request.Create().WithPath(s => Regex.IsMatch(s,"/searchapprentices/index"))
+            .UsingGet()
+        ).RespondWith(
+            Response.Create()
+                .WithStatusCode(200)
+                .WithHeader("Content-Type", "application/json")
+                .WithBodyFromFile("search-apprentices-index.json"));
+        
+        server.Given(Request.Create().WithPath(s=>Regex.IsMatch(s, @"/locations")).UsingGet()).RespondWith(Response.Create()
+            .WithStatusCode(200)
+            .WithHeader("Content-Type", "application/json")
+            .WithBodyFromFile("locations.json"));
+        return server;
+    }
+}
