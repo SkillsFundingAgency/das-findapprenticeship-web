9e511c233db45cf600f86383b6b53bb19468a638
A-J-Wright <alissa.wright@education.gov.uk>
2023-09-21 15:12:34 +0100

    view model tests

--- src/SFA.DAS.FAA.Web/Models/SearchApprenticeshipsViewModel.cs
+++ src/SFA.DAS.FAA.Web/Models/SearchApprenticeshipsViewModel.cs
@@ -1,9 +1,24 @@
-﻿namespace SFA.DAS.FAA.Web.Models;
+﻿using System.Xml.Schema;
+using SFA.DAS.FAA.Application.Vacancies.Queries;
+
+namespace SFA.DAS.FAA.Web.Models;
 
 public class SearchApprenticeshipsViewModel
 {
     public int Total { get; set; }
-    public string FormattedTotal => Total.ToString("N0");
+    public string TotalText { get; set; }
 
+    public static implicit operator SearchApprenticeshipsViewModel(GetSearchApprenticeshipsIndexResult source)
+    {
+        string formattedTotal = source.Total.ToString("N0");
+        string vacanciesText = source.Total == 1 ? "apprenticeship" : "apprenticeships";
+
+        return new SearchApprenticeshipsViewModel
+        {
+            Total = source.Total,
+            TotalText = $"{formattedTotal} {vacanciesText}"
+        };
+    }
 
 }
+
