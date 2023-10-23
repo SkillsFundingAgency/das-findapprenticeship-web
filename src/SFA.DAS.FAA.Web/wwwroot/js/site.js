bf5c40925ae43deb36eb0d9dbe20213d1089233a
James King <hello@jamesmichaelking.com>
2023-10-23 12:06:19 +0100

    Autocomplete for locations

--- src/SFA.DAS.FAA.Web/wwwroot/js/site.js
+++ src/SFA.DAS.FAA.Web/wwwroot/js/site.js
@@ -1,52 +1,50 @@
-﻿// AUTOCOMPLETE
+﻿const locationInputs = document.querySelectorAll(".faa-location-autocomplete");
+const apiUrl = "/locations";
 
-var $keywordsInput = $('#CityOrPostcode');
-var $submitOnConfirm = $('#CityOrPostcode').data('submit-on-selection');
-var $defaultValue = $('#CityOrPostcode').data('default-value');
-if ($keywordsInput.length > 0) {
-    $keywordsInput.wrap('<div id="autocomplete-container" class="das-autocomplete-wrap"></div>');
-    var container = document.querySelector('#autocomplete-container');
-    var apiUrl = '/locations';
-    $(container).empty();
-    function getSuggestions(query, updateResults) {
-        var results = [];
-        $.ajax({
-            url: apiUrl,
-            type: "get",
-            dataType: 'json',
-            data: { searchTerm: query }
-        }).done(function (data) {
-            results = data.locations.map(function (r) {
-                return r.name;
-            });
-            updateResults(results);
-        });
-    }
-    function onConfirm() {
-        var form = this.element.parentElement.parentElement;
-        var form2 = this.element.parentElement.parentElement.parentElement;
-        setTimeout(function () {
-            if (form.tagName.toLocaleLowerCase() === 'form' && $submitOnConfirm) {
-                form.submit()
-            }
-            if (form2.tagName.toLocaleLowerCase() === 'form' && $submitOnConfirm) {
-                form2.submit()
-            }
-        }, 200, form);
-    }
+if (locationInputs.length > 0) {
+  for (let i = 0; i < locationInputs.length; i++) {
+    const input = locationInputs[i];
+    const container = document.createElement("div");
+
+    container.className = "das-autocomplete-wrap";
+    input.parentNode.replaceChild(container, input);
+
+    const getSuggestions = (query, updateResults) => {
+      const results = [];
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          let results = JSON.parse(xhr.responseText);
+          results = results.locations.locations.map(function (r) {
+            return r.name;
+          });
+          updateResults(results);
+        }
+      };
+      xhr.open("GET", `${apiUrl}?searchTerm=${query}`, true);
+      xhr.send();
+    };
 
     accessibleAutocomplete({
-        element: container,
-        id: 'CityOrPostcode',
-        name: 'location',
-        displayMenu: 'overlay',
-        showNoOptionsFound: false,
-        minLength: 2,
-        source: getSuggestions,
-        placeholder: "",
-        onConfirm: onConfirm,
-        defaultValue: $defaultValue,
-        confirmOnBlur: false,
-        autoselect: true
+      element: container,
+      id: input.id,
+      name: input.name,
+      defaultValue: input.value,
+      displayMenu: "overlay",
+      showNoOptionsFound: false,
+      minLength: 2,
+      source: getSuggestions,
+      placeholder: "",
+      confirmOnBlur: false,
+      autoselect: true,
     });
+  }
+
+  const autocompleteInputs = document.querySelectorAll(".autocomplete__input");
+  if (autocompleteInputs.length > 0) {
+    for (let i = 0; i < autocompleteInputs.length; i++) {
+      const autocompleteInput = autocompleteInputs[i];
+      autocompleteInput.setAttribute("autocomplete", "new-password");
+    }
+  }
 }
