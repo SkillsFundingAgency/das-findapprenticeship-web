dbe0a71d9a67714ba16665fe5488324ade44f786
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-10-24 09:38:08 +0100

    FAI-943 change minLength to 3

--- src/SFA.DAS.FAA.Web/wwwroot/js/site.js
+++ src/SFA.DAS.FAA.Web/wwwroot/js/site.js
@@ -1,51 +1,50 @@
-﻿const locationInputs = document.querySelectorAll(".faa-location-autocomplete");
+﻿const locationInputs = document.querySelectorAll(".faa-location");
 const apiUrl = "/locations";
 
 if (locationInputs.length > 0) {
-  for (let i = 0; i < locationInputs.length; i++) {
-      const input = locationInputs[i];
-      console.log('line 7:' + input);
-    const container = document.createElement("div");
+    for (let i = 0; i < locationInputs.length; i++) {
+        const input = locationInputs[i];
+        const container = document.createElement("div");
 
-    container.className = "das-autocomplete-wrap";
-    input.parentNode.replaceChild(container, input);
+        container.className = "das-autocomplete-wrap";
+        input.parentNode.replaceChild(container, input);
 
-    const getSuggestions = (query, updateResults) => {
-      const results = [];
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          let results = JSON.parse(xhr.responseText);
-          results = results.locations.locations.map(function (r) {
-            return r.name;
-          });
-          updateResults(results);
-        }
-      };
-      xhr.open("GET", `${apiUrl}?searchTerm=${query}`, true);
-      xhr.send();
-    };
+        const getSuggestions = (query, updateResults) => {
+            const results = [];
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    let results = JSON.parse(xhr.responseText);
+                    results = results.locations.locations.map(function (r) {
+                        return r.name;
+                    });
+                    updateResults(results);
+                }
+            };
+            xhr.open("GET", `${apiUrl}?searchTerm=${query}`, true);
+            xhr.send();
+        };
 
-    accessibleAutocomplete({
-      element: container,
-      id: input.id,
-      name: input.name,
-      defaultValue: input.value,
-      displayMenu: "overlay",
-      showNoOptionsFound: false,
-      minLength: 2,
-      source: getSuggestions,
-      placeholder: "",
-      confirmOnBlur: false,
-      autoselect: true,
-    });
-  }
-
-  const autocompleteInputs = document.querySelectorAll(".autocomplete__input");
-  if (autocompleteInputs.length > 0) {
-    for (let i = 0; i < autocompleteInputs.length; i++) {
-      const autocompleteInput = autocompleteInputs[i];
-      autocompleteInput.setAttribute("autocomplete", "new-password");
+        accessibleAutocomplete({
+            element: container,
+            id: input.id,
+            name: input.name,
+            defaultValue: input.value,
+            displayMenu: "overlay",
+            showNoOptionsFound: false,
+            minLength: 3,
+            source: getSuggestions,
+            placeholder: "",
+            confirmOnBlur: false,
+            autoselect: true,
+        });
     }
-  }
+
+    const autocompleteInputs = document.querySelectorAll(".autocomplete__input");
+    if (autocompleteInputs.length > 0) {
+        for (let i = 0; i < autocompleteInputs.length; i++) {
+            const autocompleteInput = autocompleteInputs[i];
+            autocompleteInput.setAttribute("autocomplete", "new-password");
+        }
+    }
 }
