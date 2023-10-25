16aa6ec917710cc732f80f2e52e6b84de807a5e9
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-10-25 16:21:59 +0100

    FAI-943 revert minLength search

--- src/SFA.DAS.FAA.Web/wwwroot/js/site.js
+++ src/SFA.DAS.FAA.Web/wwwroot/js/site.js
@@ -32,7 +32,7 @@ if (locationInputs.length > 0) {
             defaultValue: input.value,
             displayMenu: "overlay",
             showNoOptionsFound: false,
-            minLength: 1,
+            minLength: 2,
             source: getSuggestions,
             placeholder: "",
             confirmOnBlur: false,
