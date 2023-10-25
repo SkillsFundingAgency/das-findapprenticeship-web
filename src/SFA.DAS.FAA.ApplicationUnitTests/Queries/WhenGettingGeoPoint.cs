c979d20069950f7bea92d110f576f902769de6cd
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-10-25 15:20:36 +0100

    FAI-943 add unit tests

---
+++ src/SFA.DAS.FAA.ApplicationUnitTests/Queries/WhenGettingGeoPoint.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FAA.Application.Queries.GetGeoPoint;
+using SFA.DAS.FAA.Domain.GeoPoint;
+using SFA.DAS.FAA.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FAA.Application.UnitTests.Queries;
+public class WhenGettingGeoPoint
+{
+    [Test, MoqAutoData]
+    public async Task Then_Returns_Results_From_Service(GeoPoint geoPoint)
+    {
+        var apiClientMock = new Mock<IApiClient>();
+        var query = new GetGeoPointQuery();
+        var handler = new GetGeoPointQueryHandler(apiClientMock.Object);
+
+        var expectedResponse = new GetGeoPointApiResponse() {  GeoPoint = geoPoint };
+        apiClientMock.Setup(client => client.Get<GetGeoPointApiResponse>(It.IsAny<GetGeoPointApiRequest>()))
+            .ReturnsAsync(expectedResponse);
+
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        using (new AssertionScope())
+        {
+            result.Should().NotBeNull();
+            result.PostCode.Should().BeEquivalentTo(expectedResponse.GeoPoint.Postcode);
+            result.Latitude.Should().Be(expectedResponse.GeoPoint.Latitude);
+            result.Longitude.Should().Be(expectedResponse.GeoPoint.Longitude);
+        }
+    }
+}
---
+++ src/SFA.DAS.FAA.DomainUnitTests/GeopointRequest/WhenBuildingTheRequest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAA.Domain.GeoPoint;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FAA.Domain.UnitTests.GeopointRequest;
+public class WhenBuildingTheRequest
+{
+    [Test, MoqAutoData]
+    public void Then_The_Url_Is_Correctly_Constructed(string postCode)
+    {
+        var actual = new GetGeoPointApiRequest(postCode);
+
+        actual.GetUrl.Should().Be($"locations/geopoint?postcode={postCode}");
+    }
+}
