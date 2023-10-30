8fea8de09eb2e117b3adaa4eeb18962c036c22b8
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2023-10-30 18:59:52 +0000

    Revert "FAI-913 remove all contents of acc tests proj"
    This reverts commit b0e33bb112451ae2c7ad77d28d6731d2a5e0861c.

---
+++ src/SFA.DAS.FAA.Web.AcceptanceTests/Infrastructure/ContextKeys.cs
@@ -0,0 +1,8 @@
+﻿//namespace SFA.DAS.FAA.Web.AcceptanceTests.Infrastructure;
+//public static class ContextKeys
+//{
+//    public const string HttpClient = nameof(HttpClient);
+//    public const string HttpResponse = nameof(HttpResponse);
+//    public const string TestServer = nameof(TestServer);
+//    public const string MockApiClient = nameof(MockApiClient);
+//}
---
+++ src/SFA.DAS.FAA.Web.AcceptanceTests/Infrastructure/CustomWebApplicationFactory.cs
@@ -0,0 +1,12 @@
+﻿//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Mvc.Testing;
+//using Microsoft.Extensions.Configuration;
+
+//namespace SFA.DAS.FAA.Web.AcceptanceTests.Infrastructure;
+//public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+//{
+//    protected override void ConfigureWebHost(IWebHostBuilder builder)
+//    {
+//        builder.ConfigureAppConfiguration(configurationBuilder => configurationBuilder.AddConfiguration(ConfigBuilder.GenerateConfiguration()));
+//    }
+//}
