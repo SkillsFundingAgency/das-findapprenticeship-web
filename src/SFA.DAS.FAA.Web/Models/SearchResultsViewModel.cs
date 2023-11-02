3f0fdd780ceaf944f9f0a69632f6a56f1c11dc2d
A-J-Wright <alissa.wright@education.gov.uk>
2023-11-02 09:07:30 +0000

    add attributes to view model

--- src/SFA.DAS.FAA.Web/Models/SearchResultsViewModel.cs
+++ src/SFA.DAS.FAA.Web/Models/SearchResultsViewModel.cs
@@ -5,4 +5,6 @@ public class SearchResultsViewModel
     public bool NationalSearch { get; set; }
     public string? location { get; set; }
     public List<string>? SelectedRouteIds { get; set; }
+    public string? WhatSearchTerm { get; set; }
+    public int Total { get; set; }
 }
