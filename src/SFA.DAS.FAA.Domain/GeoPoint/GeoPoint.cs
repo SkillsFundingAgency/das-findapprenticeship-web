f5c4dc3e6cf12e57e451a2ed52f06b88a2015be0
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2023-11-07 09:03:57 +0000

    FAI-945 - Remove not needed handler and request

--- src/SFA.DAS.FAA.Domain/GeoPoint/GeoPoint.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SFA.DAS.FAA.Domain.GeoPoint;
-public class GeoPoint
-{
-    public string? Postcode { get; set; }
-    public double? Latitude { get; set; }
-    public double? Longitude { get; set; }
-}
