9513916ccc7da29ced19bde4ae4799c824a05be7
A-J-Wright <alissa.wright@education.gov.uk>
2023-12-07 14:17:08 +0000

    deleting unused classes

--- src/SFA.DAS.FAA.Application/Queries/SearchApprenticeshipsIndex/GetIndexLocationQuery.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-
-namespace SFA.DAS.FAA.Application.Queries.SearchApprenticeshipsIndex;
-
-public class GetIndexLocationQuery : IRequest<GetIndexLocationQueryResult>
-{
-    public string LocationSearchTerm { get; set; }
-}
