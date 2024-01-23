f59081d0c2b3cedeb7be6dc3fd1de9b52193004d
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-01-23 09:30:20 +0000

    FAI-1099 update claims handler

--- src/SFA.DAS.FAA.Web/AppStart/CandidateAccountPostAuthenticationClaimsHandler.cs
+++ src/SFA.DAS.FAA.Web/AppStart/CandidateAccountPostAuthenticationClaimsHandler.cs
@@ -1,13 +1,44 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using SFA.DAS.FAA.Domain.Candidates;
+using SFA.DAS.FAA.Domain.Interfaces;
 using SFA.DAS.GovUK.Auth.Services;
 
 namespace SFA.DAS.FAA.Web.AppStart;
 
 public class CandidateAccountPostAuthenticationClaimsHandler : ICustomClaims
 {
+    private readonly IApiClient _apiClient;
+
+    public CandidateAccountPostAuthenticationClaimsHandler(IApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
     public async Task<IEnumerable<Claim>> GetClaims(TokenValidatedContext ctx)
     {
+        //var claims = new List<Claim>();
+        //var userId = ctx.Principal.Claims
+        //        .First(c => c.Type.Equals(ClaimTypes.NameIdentifier))
+        //        .Value;
+        //var email = ctx.Principal.Claims
+        //        .First(c => c.Type.Equals(ClaimTypes.Email))
+        //        .Value;
+        //var mobile = ctx.Principal.Claims
+        //.First(c => c.Type.Equals(ClaimTypes.MobilePhone))
+        //.Value;
+        //var candidateId = ctx.Principal.Claims
+        //    .First(c => c.Type.Equals(CustomClaims.CandidateId))
+        //    .Value;
+
+        //var requestData = new PutCandidateApiRequestData()
+        //{
+        //    Email = email,
+        //    MobilePhone = mobile
+        //};
+        //var candidate = await _apiClient.Put<PutCandidateApiResponse>(new PutCandidateApiRequest(Guid.NewGuid(), requestData));
+
+        //// add claims
+
         return new List<Claim>();
     }
 }
