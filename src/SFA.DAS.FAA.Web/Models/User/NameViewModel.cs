c7bd9d9291964ff9d409c3741ccdce5ed3f4fad2
A-J-Wright <alissa.wright@education.gov.uk>
2024-01-24 09:30:29 +0000

    add view and view model

---
+++ src/SFA.DAS.FAA.Web/Models/User/NameViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.FAA.Web.Models.User
+{
+    public class NameViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
