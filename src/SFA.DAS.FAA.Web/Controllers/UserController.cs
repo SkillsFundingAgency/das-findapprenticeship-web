bf3649955877afc0c24ba7946ab9e2d7a4bbd680
A-J-Wright <alissa.wright@education.gov.uk>
2024-01-24 13:25:48 +0000

    add controller and mediator (not finished)

--- src/SFA.DAS.FAA.Web/Controllers/UserController.cs
+++ src/SFA.DAS.FAA.Web/Controllers/UserController.cs
@@ -1,14 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.FAA.Application.Commands.UserName;
 using SFA.DAS.FAA.Web.Infrastructure;
+using SFA.DAS.FAA.Web.Models.User;
+using System.Runtime.InteropServices;
 
 namespace SFA.DAS.FAA.Web.Controllers
 {
-    public class UserController : Controller
+    public class UserController(IMediator mediator) : Controller
     {
         [Route("user-name", Name = RouteNames.UserName)]
         public IActionResult Name()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("user-name", Name = RouteNames.UserName)]
+        public async Task<IActionResult> Name([FromQuery] string firstName, [FromQuery] string lastName) 
+        {
+            var command = new UpdateNameCommand
+            {
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            await mediator.Send(command);
+
+            //TODO add correct route 
+            return RedirectToRoute("/"); //check if it has been sucessful? 
+            
         }
     }
 }
