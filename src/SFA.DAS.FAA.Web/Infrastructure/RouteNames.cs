b4bae62530a9dd9baa2ee7a01e260983f7d8ef3b
A-J-Wright <alissa.wright@education.gov.uk>
2024-01-24 09:53:23 +0000

    add controller

--- src/SFA.DAS.FAA.Web/Infrastructure/RouteNames.cs
+++ src/SFA.DAS.FAA.Web/Infrastructure/RouteNames.cs
@@ -8,7 +8,6 @@ public static class RouteNames
     public const string SearchResults = "search-results";
     public const string Vacancies = "vacancies";
     public const string Apply = "apply";
-    public const string SignOut = "sign-out";
     public const string SignedOut = "signed-out";
     public const string AccountUnavailable = "account-unavailable";
     public const string StubAccountDetailsGet = "account-details-get";
