89efffd1a0181c5d9bf0c41bd8f8b31966a9bb31
A-J-Wright <alissa.wright@education.gov.uk>
2024-01-29 10:47:09 +0000

    Add Tests

--- src/SFA.DAS.FAA.ApplicationUnitTests/Commands/UserName/WhenHandlingUpdateNameCommand.cs
+++ src/SFA.DAS.FAA.ApplicationUnitTests/Commands/UserName/WhenHandlingUpdateNameCommand.cs
@@ -25,11 +25,11 @@ namespace SFA.DAS.FAA.Application.UnitTests.Commands.UserName
 
             // Arrange
             var request = new UpdateNameApiRequest(command.GovIdentifier, new UpdateNameApiRequestData
-            {
+                {
                 Email = command.Email,
                 FirstName = command.FirstName,
                 LastName = command.LastName
-            });
+                });
             apiClientMock.Setup(x => x.PutWithResponseCode<NullResponse>(It.Is<UpdateNameApiRequest>(c=> 
                     c.PutUrl.Equals(request.PutUrl)
                         && ((UpdateNameApiRequestData)c.Data).FirstName.Equals(command.FirstName)
