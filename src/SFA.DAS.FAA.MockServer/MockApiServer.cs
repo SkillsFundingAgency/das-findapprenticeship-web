95a56370e76d75d92674948b894f60aa34218b43
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-01-29 12:46:52 +0000

    FAI-1111 - Update mock server

--- src/SFA.DAS.FAA.MockServer/MockApiServer.cs
+++ src/SFA.DAS.FAA.MockServer/MockApiServer.cs
@@ -120,6 +120,12 @@ public static class MockApiServer
                  Response.Create()
                      .WithStatusCode(202)
                      .WithBodyFromFile("put-candidate.json"));
+
+                .UsingPut())
+            .RespondWith(
+                Response.Create()
+                    .WithStatusCode(202)
+                    .WithBodyFromFile("put-candidate.json"));
         
         server.Given(Request.Create().WithPath(s => Regex.IsMatch(s, "/jobs", RegexOptions.None, regexMaxTimeOut))
                 .UsingGet())
