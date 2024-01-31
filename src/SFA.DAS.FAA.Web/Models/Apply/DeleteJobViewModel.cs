12dbc16a44e5832cd4a34431e6a8132c32502b10
A-J-Wright <alissa.wright@education.gov.uk>
2024-01-31 10:59:12 +0000

    Add delete view model

---
+++ src/SFA.DAS.FAA.Web/Models/Apply/DeleteJobViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.FAA.Web.Models.Apply
+{
+    public class DeleteJobViewModel
+    {
+        public Guid Id { get; set; }
+        public string? Employer { get; set; }
+        public string? JobTitle { get; set; }
+        public string? JobDates { get; set; }
+        public string? Description { get; set; }
+    }
+}
