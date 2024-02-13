31862ca451f1dfb7974fc100e82e4233fe91d0d1
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-02-13 13:08:45 +0000

    FAI-1092 add unit test for EditTrainingCourseViewModel

---
+++ src/SFA.DAS.FAA.Web.UnitTests/Validators/WhenValidatingEditTrainingCourse.cs
@@ -0,0 +1,85 @@
+﻿using FluentValidation.TestHelper;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FAA.Web.Models.Apply;
+using SFA.DAS.FAA.Web.Validators;
+using SFA.DAS.FAT.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FAA.Web.UnitTests.Validators;
+public class WhenValidatingEditTrainingCourse
+{
+    private const string CourseNameErrorMessage = "Enter the name of this training course";
+    private const string YearAchievedErrorMessage = "Enter the year you finished this training course";
+    private const string YearAchievedInvalidFormatErrorMessage = "Enter a real year";
+    private const string YearAchievedIsInTheFutureErrorMessage = "The year you finished this training course must be in the past";
+
+    [Test, MoqInlineAutoData(null, "2014", CourseNameErrorMessage, false)]
+    [MoqInlineAutoData("", "2014", CourseNameErrorMessage, false)]
+    public async Task AndCourseNameInvalid_ValidationResultIsInvalid(string? courseName, string? yearAchieved, string? errorMessage, bool isValid, Mock<IDateTimeService> dateTimeService)
+    {
+        var model = new EditTrainingCourseViewModel()
+        {
+            ApplicationId = Guid.NewGuid(),
+            CourseName = courseName,
+            YearAchieved = yearAchieved
+        };
+
+        var sut = new EditTrainingCourseViewModelValidator(dateTimeService.Object);
+        var result = await sut.TestValidateAsync(model);
+
+        if (!isValid)
+        {
+            result.ShouldHaveValidationErrorFor(c => c.CourseName)
+                .WithErrorMessage(errorMessage);
+        }
+        else
+        {
+            result.ShouldNotHaveValidationErrorFor(c => c.CourseName);
+        }
+    }
+
+    [Test, MoqInlineAutoData("course name", null, YearAchievedErrorMessage, false)]
+    [MoqInlineAutoData("course name", "abc", YearAchievedInvalidFormatErrorMessage, false)]
+    [MoqInlineAutoData("course name", "123", YearAchievedInvalidFormatErrorMessage, false)]
+    [MoqInlineAutoData("course name", "20345", YearAchievedInvalidFormatErrorMessage, false)]
+    [MoqInlineAutoData("course name", "2035", YearAchievedIsInTheFutureErrorMessage, false)]
+    public async Task AndYearAchievedIsInvalid_ValidationResultIsInvalid(string? courseName, string? yearAchieved, string? errorMessage, bool isValid, Mock<IDateTimeService> dateTimeService)
+    {
+        var model = new EditTrainingCourseViewModel()
+        {
+            ApplicationId = Guid.NewGuid(),
+            CourseName = courseName,
+            YearAchieved = yearAchieved
+        };
+
+        var sut = new EditTrainingCourseViewModelValidator(dateTimeService.Object);
+        var result = await sut.TestValidateAsync(model);
+
+        if (!isValid)
+        {
+            result.ShouldHaveValidationErrorFor(c => c.YearAchieved)
+                .WithErrorMessage(errorMessage);
+        }
+        else
+        {
+            result.ShouldNotHaveValidationErrorFor(c => c.YearAchieved);
+        }
+    }
+
+    [Test, MoqInlineAutoData("course name", "2022", null, true)]
+    public async Task AndInputIsValid_ValidationResultIsValid(string? courseName, string? yearAchieved, string? errorMessage, bool isValid, Mock<IDateTimeService> dateTimeService)
+    {
+        var model = new EditTrainingCourseViewModel()
+        {
+            ApplicationId = Guid.NewGuid(),
+            CourseName = courseName,
+            YearAchieved = yearAchieved
+        };
+
+        var sut = new EditTrainingCourseViewModelValidator(dateTimeService.Object);
+        var result = await sut.TestValidateAsync(model);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
