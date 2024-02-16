8d6cb4911eadbd31f61030234f4fdedf60ca8c74
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-02-16 15:50:44 +0000

    FAI-1092 merge conflicts

---
+++ src/SFA.DAS.FAA.ApplicationUnitTests/Queries/Apply/WhenHandlingGetDeleteJobQuery.cs
@@ -0,0 +1,44 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FAA.Application.Queries.Apply.GetDeleteJob;
+using SFA.DAS.FAA.Application.Queries.Apply.GetJob;
+using SFA.DAS.FAA.Domain.Apply.WorkHistory;
+using SFA.DAS.FAA.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.FAA.Application.UnitTests.Queries.Apply
+{
+    public class WhenHandlingGetDeleteJobQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Result_Is_Returned(
+          GetDeleteJobQuery query,
+          GetDeleteJobApiResponse apiResponse,
+          [Frozen] Mock<IApiClient> apiClientMock,
+          GetDeleteJobQueryHandler handler)
+        {
+            // Arrange
+            var apiRequestUri = new GetDeleteJobApiRequest(query.ApplicationId, query.CandidateId, query.JobId);
+
+            apiClientMock.Setup(client =>
+                    client.Get<GetDeleteJobApiResponse>(
+                        It.Is<GetDeleteJobApiRequest>(c =>
+                            c.GetUrl == apiRequestUri.GetUrl)))
+                .ReturnsAsync(apiResponse);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(apiResponse);
+        }
+    }
+}
---
+++ src/SFA.DAS.FAA.Web.UnitTests/Controllers/Apply/WorkHistory/WhenGettingDeleteAJob.cs
@@ -0,0 +1,50 @@
+﻿using AutoFixture.NUnit3;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FAA.Application.Queries.Apply.GetDeleteJob;
+using SFA.DAS.FAA.Application.Queries.BrowseByInterests;
+using SFA.DAS.FAA.Web.AppStart;
+using SFA.DAS.FAA.Web.Models;
+using SFA.DAS.FAA.Web.Models.Apply;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.FAA.Web.UnitTests.Controllers.Apply.WorkHistory
+{
+    public class WhenGettingDeleteAJob
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Mediator_Query_Is_Called_And_Delete_Jov_View_Is_Returned(
+        Guid candidateId,
+        Guid applicationId,
+        Guid jobId,
+        GetDeleteJobQueryResult result,
+        [Frozen] Mock<IMediator> mediator,
+        [Greedy] Web.Controllers.Apply.WorkHistoryController controller)
+        {
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                        { new(CustomClaims.CandidateId, candidateId.ToString()) }))
+                }
+            };
+            mediator.Setup(x => x.Send(It.IsAny<GetDeleteJobQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(result);
+
+            var actual = await controller.GetDeleteJob(applicationId, jobId) as ViewResult;
+
+            Assert.That(actual, Is.Not.Null);
+            var actualModel = actual!.Model as DeleteJobViewModel;
+        }
+    }
+}
