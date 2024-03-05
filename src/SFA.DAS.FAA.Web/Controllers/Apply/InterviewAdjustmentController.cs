f3b262c671daf8f6dd2a0551bae1fb99a2b946db
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2024-03-05 10:38:57 +0000

    Controller added for the interviewAdjustment

---
+++ src/SFA.DAS.FAA.Web/Controllers/Apply/InterviewAdjustmentController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.FAA.Web.Authentication;
+
+namespace SFA.DAS.FAA.Web.Controllers.Apply
+{
+    [Authorize(Policy = nameof(PolicyNames.IsFaaUser))]
+    public class InterviewAdjustmentController(IMediator mediator) : Controller
+    {
+        public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
