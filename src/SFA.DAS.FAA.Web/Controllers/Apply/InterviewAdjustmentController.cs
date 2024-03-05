3a9d00d4d5e1b8d8eb1b6a3510ecc205edf626d6
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2024-03-05 16:30:39 +0000

    Controller and view added

--- src/SFA.DAS.FAA.Web/Controllers/Apply/InterviewAdjustmentController.cs
+++ src/SFA.DAS.FAA.Web/Controllers/Apply/InterviewAdjustmentController.cs
@@ -2,13 +2,16 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.FAA.Web.Authentication;
+using SFA.DAS.FAA.Web.Infrastructure;
 
 namespace SFA.DAS.FAA.Web.Controllers.Apply
 {
     [Authorize(Policy = nameof(PolicyNames.IsFaaUser))]
     public class InterviewAdjustmentController(IMediator mediator) : Controller
     {
-        public IActionResult Summary()
+        [HttpGet]
+        [Route("apply/{applicationId}/interview-adjustment/summary", Name = RouteNames.ApplyApprenticeship.InterviewAdjustmentSummary)]
+        public IActionResult Summary([FromRoute] Guid applicationId)
         {
             return View();
         }
---
+++ src/SFA.DAS.FAA.Web/Models/Apply/InterviewQuestionSummaryViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.FAA.Domain.Enums;
+
+namespace SFA.DAS.FAA.Web.Models.Apply
+{
+    public class InterviewQuestionSummaryViewModel : ViewModelBase
+    {
+        [FromRoute]
+        public required Guid ApplicationId { get; init; }
+        public string? BackLinkUrl { get; set; }
+        public string? ChangeLinkUrl { get; set; }
+        public string? ChangeSupportRequestAnswerLinkUrl { get; set; }
+        public bool IsSupportRequestRequired { get; set; }
+        public string? SupportRequestAnswer { get; set; }
+
+        [BindProperty]
+        public SectionStatus? IsSectionCompleted { get; set; }
+        public List<SectionProgress> InterviewQuestionCompletionStatus { get; init; } =
+        [
+            new SectionProgress
+            {
+                Id = SectionStatus.Completed,
+                StatusLabel = "Yes, I’ve completed this section"
+            },
+
+            new SectionProgress
+            {
+                Id = SectionStatus.InProgress,
+                StatusLabel = "No, I’ll come back to it later"
+            }
+        ];
+    }
+}
