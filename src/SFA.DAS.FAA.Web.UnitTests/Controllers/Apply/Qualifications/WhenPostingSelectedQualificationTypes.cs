30fefda68e90870c048ea0f109b4ef392108c3ee
chrisfoster76 <chrisfoster186@googlemail.com>
2024-03-19 19:31:22 +0000

    Test fixes

--- src/SFA.DAS.FAA.Web.UnitTests/Controllers/Apply/Qualifications/WhenPostingSelectedQualificationTypes.cs
+++ src/SFA.DAS.FAA.Web.UnitTests/Controllers/Apply/Qualifications/WhenPostingSelectedQualificationTypes.cs
@@ -1,14 +1,17 @@
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.FAA.Application.Queries.Apply.GetQualificationTypes;
+using SFA.DAS.FAA.Web.AppStart;
 using SFA.DAS.FAA.Web.Controllers.Apply;
 using SFA.DAS.FAA.Web.Infrastructure;
 using SFA.DAS.FAA.Web.Models.Apply;
 using SFA.DAS.Testing.AutoFixture;
+using System.Security.Claims;
 
 namespace SFA.DAS.FAA.Web.UnitTests.Controllers.Apply.Qualifications;
 
@@ -18,12 +21,22 @@ public class WhenPostingSelectedQualificationTypes
     public async Task Then_If_Nothing_Selected_Then_Error_Returned(
         AddQualificationSelectTypeViewModel model,
         GetQualificationTypesQueryResponse queryResult,
+        Guid candidateId,
         [Frozen] Mock<IMediator> mediator,
         [Greedy] QualificationsController controller)
     {
         model.QualificationReferenceId = Guid.Empty;
-        mediator.Setup(x => x.Send(It.Is<GetQualificationTypesQuery>(c=>c.ApplicationId == model.ApplicationId), CancellationToken.None))
+        mediator.Setup(x => x.Send(It.Is<GetQualificationTypesQuery>(c=>c.ApplicationId == model.ApplicationId && c.CandidateId == candidateId), CancellationToken.None))
             .ReturnsAsync(queryResult);
+
+        controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                    { new(CustomClaims.CandidateId, candidateId.ToString()) }))
+            }
+        };
 
         var actual = await controller.AddQualificationSelectTypePost(model) as ViewResult;
 
