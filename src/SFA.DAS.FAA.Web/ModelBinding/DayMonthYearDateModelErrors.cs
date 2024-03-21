6216dd30dd83d8be039d91825c272a1e29f95ecb
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-03-21 11:31:03 +0000

    FAI-1104 update validation errors

--- src/SFA.DAS.FAA.Web/ModelBinding/DayMonthYearDateModelErrors.cs
+++ src/SFA.DAS.FAA.Web/ModelBinding/DayMonthYearDateModelErrors.cs
@@ -3,8 +3,8 @@
 public static class DayMonthYearDateModelErrors
 {
     public const string DateOfBirthIncomplete = "Date of birth must include a day, month and year";
-    public const string InvalidYear = "Year must include 4 numbers";
-    public const string InvalidMonth = "Month must include 2 numbers";
-    public const string InvalidDay = "Day must include 2 numbers";
+    public const string InvalidYear = "Enter a real year";
+    public const string InvalidMonth = "Month must be 2 characters or less";
+    public const string InvalidDay = "Day must be 2 characters or less";
     public const string InvalidDate = "Date of birth must be a real date";
 }
