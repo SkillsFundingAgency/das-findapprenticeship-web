2ad177cb58129371420709358d558b0e32d6f3a9
chrisfoster76 <chrisfoster186@googlemail.com>
2024-03-25 13:53:12 +0000

    Adds explicit mapper tests around grouping

--- src/SFA.DAS.FAA.Web/Models/Apply/QualificationsViewModel.cs
+++ src/SFA.DAS.FAA.Web/Models/Apply/QualificationsViewModel.cs
@@ -5,6 +5,38 @@ namespace SFA.DAS.FAA.Web.Models.Apply
 {
     public class QualificationsViewModel
     {
+        [FromRoute]
+        public required Guid ApplicationId { get; set; }
+
+        public bool? DoYouWantToAddAnyQualifications { get; set; }
+
+        public bool? IsSectionCompleted { get; set; }
+
+        public List<QualificationGroup> QualificationGroups { get; set; } = new List<QualificationGroup>();
+
+        public bool ShowQualifications { get; set; }
+
+        public class QualificationGroup
+        {
+            public Guid QualificationReferenceId { get; set; }
+            public string DisplayName { get; set; }
+            public List<Qualification> Qualifications { get; set; } = new List<Qualification>();
+            public bool AllowMultipleAdd { get; set; }
+            public bool ShowAdditionalInformation { get; set; }
+        }
+
+        public class Qualification
+        {
+            public Guid? Id { get; set; }
+            public string Subject { get; set; }
+            public string Grade { get; set; }
+            public string Level { get; set; }
+            public string AdditionalInformation { get; set; }
+            public bool? IsPredicted { get; set; }
+
+            public string GradeLabel => IsPredicted is true ? $"{Grade} (predicted)" : Grade;
+        }
+
         public static QualificationsViewModel MapFromQueryResult(Guid applicationId, GetQualificationsQueryResult source)
         {
             var result = new QualificationsViewModel
@@ -41,38 +73,6 @@ namespace SFA.DAS.FAA.Web.Models.Apply
             }
 
             return result;
-        }
-
-        [FromRoute]
-        public required Guid ApplicationId { get; set; }
-
-        public bool? DoYouWantToAddAnyQualifications { get; set; }
-
-        public bool? IsSectionCompleted { get; set; }
-
-        public List<QualificationGroup> QualificationGroups { get; set; } = new List<QualificationGroup>();
-
-        public bool ShowQualifications { get; set; }
-
-        public class QualificationGroup
-        {
-            public Guid QualificationReferenceId { get; set; }
-            public string DisplayName { get; set; }
-            public List<Qualification> Qualifications { get; set; } = new List<Qualification>();
-            public bool AllowMultipleAdd { get; set; }
-            public bool ShowAdditionalInformation { get; set; }
-        }
-
-        public class Qualification
-        {
-            public Guid? Id { get; set; }
-            public string Subject { get; set; }
-            public string Grade { get; set; }
-            public string Level { get; set; }
-            public string AdditionalInformation { get; set; }
-            public bool? IsPredicted { get; set; }
-
-            public string GradeLabel => IsPredicted is true ? $"{Grade} (predicted)" : Grade;
         }
 
         private static QualificationGroup MapGroup(QualificationDisplayTypeViewModel qualificationType,
