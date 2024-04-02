78421b7f5739188c92b3a21231d3feaef77c6246
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-04-02 11:37:28 +0100

    Unit test added for the Implicit Operator

---
+++ src/SFA.DAS.FAA.Web.UnitTests/Models/Apply/WhenMappingMediatrResponseToApplicationSubmittedConfirmationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAA.Application.Queries.Apply.GetApplicationSubmitted;
+using SFA.DAS.FAA.Web.Models.Apply;
+
+namespace SFA.DAS.FAA.Web.UnitTests.Models.Apply
+{
+    public class WhenMappingMediatrResponseToApplicationSubmittedConfirmationViewModel
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(GetApplicationSubmittedQueryResponse source)
+        {
+            var actual = (ApplicationSubmittedVacancyInfo)source;
+
+            actual.Should().BeEquivalentTo(source);
+        }
+    }
+}
