6414bdef716aec9f036d0b4e5004a909db92c9c5
chrisfoster76 <chrisfoster186@googlemail.com>
2024-04-15 10:29:44 +0100

    Placeholders

---
+++ src/SFA.DAS.FAA.Web/Controllers/ApplicationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.FAA.Web.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
