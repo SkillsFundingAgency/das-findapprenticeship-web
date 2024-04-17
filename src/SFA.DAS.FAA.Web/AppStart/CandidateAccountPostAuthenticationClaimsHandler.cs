1fb24b6c2d8340eb3d6ecdbdb2269db5e670b054
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-04-17 16:39:18 +0100

    reverted the changes

--- src/SFA.DAS.FAA.Web/AppStart/CandidateAccountPostAuthenticationClaimsHandler.cs
+++ src/SFA.DAS.FAA.Web/AppStart/CandidateAccountPostAuthenticationClaimsHandler.cs
@@ -27,17 +27,8 @@ public class CandidateAccountPostAuthenticationClaimsHandler : ICustomClaims
         {
             Email = email
         };
-        //var candidate = await _apiClient.Put<PutCandidateApiResponse>(new PutCandidateApiRequest(userId, requestData));
-
-        var candidate = new PutCandidateApiResponse
-        {
-            Id = Guid.Parse("A7982B5E-4791-48C3-F5F6-08DC1E861F30"),
-            FirstName = "Balaji",
-            LastName = "Jambulingam",
-            Email = email,
-            GovUkIdentifier = "897f2117-8c01-4417-96ff-372c03ea499a"
-        };
-
+        var candidate = await _apiClient.Put<PutCandidateApiResponse>(new PutCandidateApiRequest(userId, requestData));
+        
         // add claims
 
         var claims = new List<Claim>{new Claim(CustomClaims.CandidateId, candidate.Id.ToString())};
