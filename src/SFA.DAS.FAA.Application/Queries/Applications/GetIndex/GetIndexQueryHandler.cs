53ee2290c27761e8f0c11df1a69ea83a882b3287
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-05-30 22:45:03 +0100

    Banner Message added

--- src/SFA.DAS.FAA.Application/Queries/Applications/GetIndex/GetIndexQueryHandler.cs
+++ src/SFA.DAS.FAA.Application/Queries/Applications/GetIndex/GetIndexQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using SFA.DAS.FAA.Domain.Applications.GetApplications;
 using SFA.DAS.FAA.Domain.Interfaces;
+using SFA.DAS.FAA.Domain.User;
 
 namespace SFA.DAS.FAA.Application.Queries.Applications.GetIndex;
 
@@ -8,9 +9,30 @@ public class GetIndexQueryHandler(IApiClient apiClient) : IRequestHandler<GetInd
 {
     public async Task<GetIndexQueryResult> Handle(GetIndexQuery request, CancellationToken cancellationToken)
     {
-        var response = await apiClient.Get<GetApplicationsApiResponse>(
+        var applicationsTask = apiClient.Get<GetApplicationsApiResponse>(
             new GetApplicationsApiRequest(request.CandidateId, request.Status));
 
-        return response;
+        var candidateTask = apiClient.Get<GetInformApiResponse>(new GetInformApiRequest(request.CandidateId));
+
+        await Task.WhenAll(applicationsTask, candidateTask);
+
+        return new GetIndexQueryResult
+        {
+            Applications = applicationsTask.Result.Applications.Select(x => new GetIndexQueryResult.Application
+            {
+                Id = x.Id,
+                Title = x.Title,
+                VacancyReference = x.VacancyReference,
+                EmployerName = x.EmployerName,
+                CreatedDate = x.CreatedDate,
+                ClosingDate = x.ClosingDate,
+                SubmittedDate = x.SubmittedDate,
+                WithdrawnDate = x.WithdrawnDate,
+                ResponseDate = x.ResponseDate,
+                Status = x.Status,
+                ResponseNotes = x.ResponseNotes
+            }).ToList(),
+            ShowAccountRecoveryBanner = candidateTask.Result.ShowAccountRecoveryBanner
+        };
     }
 }
