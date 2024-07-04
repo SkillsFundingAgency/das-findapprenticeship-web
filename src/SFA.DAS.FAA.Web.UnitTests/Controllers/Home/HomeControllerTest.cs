ddd172118df810dc301a043cf5b3e9b3d4d9d86b
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-07-04 11:02:55 +0100

    Renaming the Cookie, Fixing the build failures

--- src/SFA.DAS.FAA.Web.UnitTests/Controllers/Home/HomeControllerTest.cs
+++ src/SFA.DAS.FAA.Web.UnitTests/Controllers/Home/HomeControllerTest.cs
@@ -1,14 +1,11 @@
-﻿using AutoFixture.NUnit3;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.FAA.Web.Controllers;
-using SFA.DAS.FAA.Web.Infrastructure;
 using SFA.DAS.FAA.Web.Models;
 using SFA.DAS.Testing.AutoFixture;
-using DefaultHttpContext = Microsoft.AspNetCore.Http.DefaultHttpContext;
 
 namespace SFA.DAS.FAA.Web.UnitTests.Controllers.Home
 {
@@ -21,8 +18,8 @@ namespace SFA.DAS.FAA.Web.UnitTests.Controllers.Home
             string previousPageUrl)
         {
             var cookiesMock = new Mock<IRequestCookieCollection>();
-            cookiesMock.SetupGet(c => c["AnalyticsCookieConsent"]).Returns(cookieValue.ToString);
-            cookiesMock.SetupGet(c => c["FunctionalCookieConsent"]).Returns(cookieValue.ToString);
+            cookiesMock.SetupGet(c => c["AnalyticsConsent"]).Returns(cookieValue.ToString);
+            cookiesMock.SetupGet(c => c["FunctionalConsent"]).Returns(cookieValue.ToString);
 
             var httpContextMock = new Mock<HttpContext>();
             httpContextMock.Setup(ctx => ctx.Request.Cookies).Returns(cookiesMock.Object);
