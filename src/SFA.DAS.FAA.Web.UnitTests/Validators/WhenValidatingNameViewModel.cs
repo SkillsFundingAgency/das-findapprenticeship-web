33d5f67173a50640114b4aa562097f8cfd49b1ea
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-07-09 10:47:48 +0100

    Adding NameViewModel Validator Message
    Bug: https://skillsfundingagency.atlassian.net/browse/FAI-1752

---
+++ src/SFA.DAS.FAA.Web.UnitTests/Validators/WhenValidatingNameViewModel.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.FAA.Web.Models.User;
+using SFA.DAS.FAA.Web.Validators;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FAA.Web.UnitTests.Validators
+{
+    public class WhenValidatingNameViewModel
+    {
+        private const string FirstNameErrorMessage = "Enter your first name";
+        private const string LastNameErrorMessage = "Enter your last name";
+
+        [Test]
+        [MoqInlineAutoData(null, FirstNameErrorMessage, false)]
+        [MoqInlineAutoData("", FirstNameErrorMessage, false)]
+        [MoqInlineAutoData("some name", null, true)]
+        public async Task And_FirstName_Invalid_ValidationResult_Returned(string? firstName, string? errorMessage, bool isValid)
+        {
+            var model = new NameViewModel
+            {
+                FirstName = firstName
+            };
+
+            var sut = new NameViewModelValidator();
+            var result = await sut.TestValidateAsync(model);
+
+            if (!isValid)
+            {
+                result.ShouldHaveValidationErrorFor(c => c.FirstName)
+                    .WithErrorMessage(errorMessage);
+            }
+            else
+            {
+                result.ShouldNotHaveValidationErrorFor(c => c.FirstName);
+            }
+        }
+
+        [Test]
+        [MoqInlineAutoData(null, LastNameErrorMessage, false)]
+        [MoqInlineAutoData("", LastNameErrorMessage, false)]
+        [MoqInlineAutoData("some name", null, true)]
+        public async Task And_LastName_Invalid_ValidationResult_Returned(string? lastName, string? errorMessage, bool isValid)
+        {
+            var model = new NameViewModel
+            {
+                LastName = lastName
+            };
+
+            var sut = new NameViewModelValidator();
+            var result = await sut.TestValidateAsync(model);
+
+            if (!isValid)
+            {
+                result.ShouldHaveValidationErrorFor(c => c.LastName)
+                    .WithErrorMessage(errorMessage);
+            }
+            else
+            {
+                result.ShouldNotHaveValidationErrorFor(c => c.LastName);
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.FAA.Web/Validators/NameViewModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using SFA.DAS.FAA.Web.Models.User;
+
+namespace SFA.DAS.FAA.Web.Validators
+{
+    public class NameViewModelValidator : AbstractValidator<NameViewModel>
+    {
+        private const string NoFirstName = "Enter your first name";
+        private const string NoLastName = "Enter your last name";
+
+        public NameViewModelValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage(NoFirstName);
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage(NoLastName);
+        }
+    }
+}
