2b401c56919f46bd2dac8e376335b15dc1ff6463
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-07-11 17:57:42 +0100

    Transfer your data page back link fix

--- src/SFA.DAS.FAA.Web.UnitTests/Controllers/Users/WhenGettingTransferYourData.cs
+++ src/SFA.DAS.FAA.Web.UnitTests/Controllers/Users/WhenGettingTransferYourData.cs
@@ -1,9 +1,18 @@
 ﻿using AutoFixture.NUnit3;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.FAA.Web.Controllers;
+using SFA.DAS.FAA.Web.Infrastructure;
+using SFA.DAS.FAA.Web.Models.User;
+using SFA.DAS.GovUK.Auth.Services;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.FAA.Web.UnitTests.Controllers.Users
@@ -11,15 +20,41 @@ namespace SFA.DAS.FAA.Web.UnitTests.Controllers.Users
     [TestFixture]
     public class WhenGettingTransferYourData
     {
-        [Test, MoqAutoData]
+        [Test]
+        [MoqInlineAutoData(null, "https://baseUrl/create-account")]
+        [MoqInlineAutoData("some url", "some url")]
         public void Then_View_Is_Returned(
-            [Greedy] UserController controller)
+            string previousPageUrl,
+            string expectedUrl,
+            [Frozen] Mock<IMediator> mediator,
+            [Frozen] Mock<ICacheStorageService> cacheStorageService)
         {
-            var result = controller.TransferYourData();
+            var mockUrlHelper = new Mock<IUrlHelper>();
+            mockUrlHelper
+                .Setup(x => x.RouteUrl(It.IsAny<UrlRouteContext>()))
+                .Returns(expectedUrl);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(ctx => ctx.Request.Headers.Referer).Returns(new StringValues(previousPageUrl));
+
+            var controller = new UserController(mediator.Object, cacheStorageService.Object, Mock.Of<IConfiguration>(), Mock.Of<IOidcService>())
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = httpContextMock.Object
+                },
+                Url = mockUrlHelper.Object
+            };
+
+            var result = controller.TransferYourData() as ViewResult;
 
             using var scope = new AssertionScope();
             result.Should().NotBeNull();
-            result.Should().BeOfType<ViewResult>();
+            var actualModel = result!.Model as TransferYourDataViewModel;
+
+            actualModel.Should().NotBeNull();
+            actualModel!.PreviousPageUrl.Should().NotBeNull();
+            actualModel.PreviousPageUrl.Should().Be(expectedUrl);
         }
     }
 }
---
+++ src/SFA.DAS.FAA.Web/Models/User/TransferYourDataViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.FAA.Web.Models.User
+{
+    public record TransferYourDataViewModel
+    {
+        public string? PreviousPageUrl { get; set; }
+    }
+}
