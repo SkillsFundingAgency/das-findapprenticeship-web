4ea33f371cae249bb0e06e726991cef98bacb7b8
killij <51908793+killij@users.noreply.github.com>
2024-10-21 09:30:57 +0100

    FAI-1926 - Update Your IndexViewModel to utilise an application's ClosedDate

--- src/SFA.DAS.FAA.Web.UnitTests/Models/Applications/WhenMappingIndexViewModel.cs
+++ src/SFA.DAS.FAA.Web.UnitTests/Models/Applications/WhenMappingIndexViewModel.cs
@@ -1,40 +1,87 @@
-using AutoFixture;
-using AutoFixture.NUnit3;
+using FluentAssertions.Extensions;
 using SFA.DAS.FAA.Application.Queries.Applications.GetIndex;
 using SFA.DAS.FAA.Domain.Enums;
 using SFA.DAS.FAA.Web.Models.Applications;
 using SFA.DAS.FAT.Domain.Interfaces;
 
-namespace SFA.DAS.FAA.Web.UnitTests.Models.Applications
+namespace SFA.DAS.FAA.Web.UnitTests.Models.Applications;
+
+[TestFixture]
+public class WhenMappingIndexViewModel
 {
-    public class WhenMappingIndexViewModel
+    public class WhenMappingApplication
     {
-        [Test, MoqAutoData]
-        public void Then_Applications_Are_Correctly_Split_By_Their_Status_Into_Their_Associated_Properties(
-            IFixture fixture,
-            GetIndexQueryResult source,
-            [Frozen] IDateTimeService dateTimeService)
+        IDateTimeService _dateTimeService;
+
+        [SetUp]
+        public void Setup()
         {
-            // arrange - let's make sure the application statuses are consistent 
-            source.Applications.Clear();
-            var applicationStatusValues = Enum.GetValues<ApplicationStatus>().ToList();
-            applicationStatusValues.ForEach(x =>
-            {
-                var application = fixture.Create<GetIndexQueryResult.Application>();
-                application.Status = x;
-                source.Applications.Add(application);
-            });
+            var dts = new Mock<IDateTimeService>();
+            dts.Setup(x => x.GetDateTime()).Returns(DateTime.UtcNow);
+
+            _dateTimeService = dts.Object;
+        }
+
+        [Test, MoqAutoData]
+        public void Then_A_Vacancy_Closed_Before_Today_Generates_The_Correct_Closed_Text(IFixture fixture)
+        {
+            // arrange
+            var application = fixture.Create<GetIndexQueryResult.Application>();
+            application.ClosingDate = DateTime.UtcNow.AddDays(5);
+            application.ClosedDate = DateTime.UtcNow.AddDays(-5);
+            application.Status = ApplicationStatus.Submitted;
 
             // act
-            var result = IndexViewModel.Map(ApplicationsTab.None, source, dateTimeService);
-            
+            var actual = IndexViewModel.Application.From(application, _dateTimeService);
+
             // assert
-            using (new AssertionScope())
-            {
-                result.Applications.Count.Should().Be(4);
-                result.WithdrawnApplications.Count.Should().Be(1);
-                result.ExpiredApplications.Count.Should().Be(1);
-            }
+            actual.IsClosed.Should().BeTrue();
+            actual.ClosingDate.Should().StartWith("Closed");
+        }
+
+        [Test, MoqAutoData]
+        public void Then_A_Vacancy_Closed_Early_Today_Generates_The_Correct_Closed_Text(IFixture fixture)
+        {
+            // arrange
+            var application = fixture.Create<GetIndexQueryResult.Application>();
+            application.ClosingDate = DateTime.UtcNow.AddDays(5);
+            application.ClosedDate = DateTime.UtcNow.AddSeconds(-5);
+            application.Status = ApplicationStatus.Submitted;
+
+            // act
+            var actual = IndexViewModel.Application.From(application, _dateTimeService);
+
+            // assert
+            actual.IsClosed.Should().BeTrue();
+            actual.ClosingDate.Should().StartWith("Closed");
+        }
+    }
+    
+    [Test, MoqAutoData]
+    public void Then_Applications_Are_Correctly_Split_By_Their_Status_Into_Their_Associated_Properties(
+        IFixture fixture,
+        GetIndexQueryResult source,
+        [Frozen] IDateTimeService dateTimeService)
+    {
+        // arrange - let's make sure the application statuses are consistent 
+        source.Applications.Clear();
+        var applicationStatusValues = Enum.GetValues<ApplicationStatus>().ToList();
+        applicationStatusValues.ForEach(x =>
+        {
+            var application = fixture.Create<GetIndexQueryResult.Application>();
+            application.Status = x;
+            source.Applications.Add(application);
+        });
+
+        // act
+        var result = IndexViewModel.Map(ApplicationsTab.None, source, dateTimeService);
+        
+        // assert
+        using (new AssertionScope())
+        {
+            result.Applications.Count.Should().Be(4);
+            result.WithdrawnApplications.Count.Should().Be(1);
+            result.ExpiredApplications.Count.Should().Be(1);
         }
     }
 }
