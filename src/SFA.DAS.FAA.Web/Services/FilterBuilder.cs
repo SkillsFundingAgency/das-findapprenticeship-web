6d9cc47018ff1cd8359d10ad0bb57e9c576ae8c6
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-01-22 16:16:15 +0000

    Reverting the changes

--- src/SFA.DAS.FAA.Web/Services/FilterBuilder.cs
+++ src/SFA.DAS.FAA.Web/Services/FilterBuilder.cs
@@ -25,7 +25,7 @@ namespace SFA.DAS.FAA.Web.Services
             filters.AddSingleFilterItem(urlHelper, fullQueryParameters, "What",
                 request.SearchTerm,[$"searchTerm={request.SearchTerm}"]);
             filters.AddSingleFilterItem(urlHelper, fullQueryParameters, "Where",
-                string.IsNullOrEmpty(request.Location) ? "" : $"{request.Location} ({(request.Distance != null ? $"within {request.Distance} miles" : "All of England")})",
+                string.IsNullOrEmpty(request.Location) ? "" : $"{request.Location} ({(request.Distance != null ? $"within {request.Distance} miles" : "across England")})",
                 [$"location={request.Location}", $"distance={(request.Distance == null ? "all" : request.Distance)}", $"sort={VacancySort.DistanceAsc}"]);
             
             filters.AddFilterItems(urlHelper, fullQueryParameters, request.RouteIds, "Job category", "routeIds", filterChoices.JobCategoryChecklistDetails.Lookups.ToList());
