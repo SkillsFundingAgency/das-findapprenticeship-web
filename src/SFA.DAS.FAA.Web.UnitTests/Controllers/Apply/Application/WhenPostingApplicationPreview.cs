1e39bb125af00ee096b53d5a9ceafafbc1df6fce
killij <51908793+killij@users.noreply.github.com>
2025-09-05 14:37:02 +0100

    FAI-2765 Updates for ApplicationSubmittedViewModelValidator

--- src/SFA.DAS.FAA.Web.UnitTests/Controllers/Apply/Application/WhenPostingApplicationPreview.cs
+++ src/SFA.DAS.FAA.Web.UnitTests/Controllers/Apply/Application/WhenPostingApplicationPreview.cs
@@ -1,14 +1,8 @@
-﻿using AutoFixture.NUnit3;
-using FluentAssertions;
-using FluentAssertions.Execution;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
-using Moq;
-using NUnit.Framework;
 using SFA.DAS.FAA.Application.Commands.UpdateApplication.ApplicationStatus;
-using SFA.DAS.FAA.Domain.Enums;
 using SFA.DAS.FAA.Web.AppStart;
 using SFA.DAS.FAA.Web.Controllers;
 using SFA.DAS.FAA.Web.Infrastructure;
@@ -27,6 +21,7 @@ namespace SFA.DAS.FAA.Web.UnitTests.Controllers.Apply.Application
             Guid applicationId,
             Guid candidateId,
             UpdateApplicationStatusCommandResult updateApplicationStatusCommandResult,
+            Mock<IValidator<ApplicationSummaryViewModel>> validator,
             [Frozen] Mock<IDateTimeService> dateTimeService,
             [Frozen] Mock<ICacheStorageService> cacheStorageService,
             [Frozen] Mock<IMediator> mediator)
@@ -55,8 +50,12 @@ namespace SFA.DAS.FAA.Web.UnitTests.Controllers.Apply.Application
                     }
                 }
             };
+            
+            validator
+                .Setup(x => x.ValidateAsync(It.Is<ApplicationSummaryViewModel>(m => m == request), CancellationToken.None))
+                .ReturnsAsync(new ValidationResult());
 
-            var actual = await controller.Preview(applicationId, request) as RedirectToRouteResult;
+            var actual = await controller.Preview(validator.Object, applicationId, request) as RedirectToRouteResult;
             using (new AssertionScope())
             {
                 actual.Should().NotBeNull();
