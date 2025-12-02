c19e023c627d6ca0976848e515903950207979ff
killij <51908793+killij@users.noreply.github.com>
2025-12-02 10:44:25 +0000

    FAI-2914 Add GovUkBannerTagHelper

---
+++ src/SFA.DAS.FAA.Web.UnitTests/TagHelpers/GovUkBannerTagHelperTests.cs
@@ -0,0 +1,66 @@
+﻿using SFA.DAS.FAA.Web.TagHelpers;
+
+namespace SFA.DAS.FAA.Web.UnitTests.TagHelpers;
+
+public class GovUkBannerTagHelperTests: TagHelperTestsBase
+{
+    [TestCase(BannerStyle.Success, "Success")]
+    [TestCase(BannerStyle.Important, "Important")]
+    public async Task Output_Contains_Default_Header_For_Type(BannerStyle bannerStyle, string title)
+    {
+        // arrange
+        var sut = new GovUkBannerTagHelper
+        {
+            Style = bannerStyle
+        };
+
+        // act
+        await sut.ProcessAsync(TagHelperContext, TagHelperOutput);
+
+        // assert
+        TagHelperOutput.AsString().Should().Contain($">{title}<");
+    }
+    
+    [TestCase(BannerStyle.Success, "New Title")]
+    public async Task Output_Contains_Overridden_Header_For_Type(BannerStyle bannerStyle, string title)
+    {
+        // arrange
+        var sut = new GovUkBannerTagHelper
+        {
+            Title = title,
+            Style = bannerStyle
+        };
+
+        // act
+        await sut.ProcessAsync(TagHelperContext, TagHelperOutput);
+
+        // assert
+        TagHelperOutput.AsString().Should().Contain($">{title}<");
+    }
+    
+    [Test]
+    public async Task Output_Contains_Default_Content()
+    {
+        // arrange
+        var sut = new GovUkBannerTagHelper { Style = BannerStyle.Success };
+
+        // act
+        await sut.ProcessAsync(TagHelperContext, TagHelperOutput);
+
+        // assert
+        TagHelperOutput.AsString().Should().Contain(">default content<");
+    }
+    
+    [Test]
+    public async Task Banners_Role_Is_Alert()
+    {
+        // arrange
+        var sut = new GovUkBannerTagHelper { Style = BannerStyle.Success };
+
+        // act
+        await sut.ProcessAsync(TagHelperContext, TagHelperOutput);
+
+        // assert
+        TagHelperOutput.AsString().Should().Contain("role=\"HtmlEncode[[alert]]\"");
+    }
+}
---
+++ src/SFA.DAS.FAA.Web/TagHelpers/GovUkBannerTagHelper.cs
@@ -0,0 +1,88 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SFA.DAS.FAA.Web.TagHelpers;
+
+public enum BannerStyle
+{
+    Important,
+    Success,
+}
+
+[HtmlTargetElement(TagName)]
+[OutputElementHint("div")]
+public class GovUkBannerTagHelper: TagHelper
+{
+    public const string TagName = "govuk-banner";
+    public string Id { get; set; } = $"banner_{Guid.NewGuid()}";
+    public string? Title { get; set; }
+    public BannerStyle Style { get; set; } = BannerStyle.Important;
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.AddClass("govuk-notification-banner", HtmlEncoder.Default);
+        output.Attributes.Add("aria-labelledby", Id);
+        output.Attributes.Add("data-module", "govuk-notification-banner");
+        
+        switch (Style)
+        {
+            case BannerStyle.Success:
+                {
+                    Title ??= "Success";
+                    output.AddClass("govuk-notification-banner--success", HtmlEncoder.Default);
+                    output.Attributes.Add("role", "alert");
+                    break;
+                }
+            case BannerStyle.Important:
+            default:
+                {
+                    Title ??= "Important";
+                    output.Attributes.Add("role", "region");
+                    break;
+                }
+        }
+        
+        output.Content.AppendHtml(RenderHeader(Id, Title));
+        output.Content.AppendHtml(RenderContent(await output.GetChildContentAsync()));
+    }
+
+    private static TagBuilder RenderHeader(string id, string title)
+    {
+        var header = new TagBuilder("h2");
+        header.AddCssClass("govuk-notification-banner__title");
+        header.Attributes.Add("id", id);
+        header.InnerHtml.AppendHtml(title);
+        
+        var div = new TagBuilder("div");
+        div.AddCssClass("govuk-notification-banner__header");
+        div.InnerHtml.AppendHtml(header);
+        return div;
+    }
+    
+    private static TagBuilder RenderContent(TagHelperContent content)
+    {
+        var div = new TagBuilder("div");
+        div.AddCssClass("govuk-notification-banner__content");
+        div.InnerHtml.AppendHtml(content);
+        return div;
+    }
+}
+
+[HtmlTargetElement(TagName, ParentTag = GovUkBannerTagHelper.TagName)]
+[OutputElementHint("h3")]
+public class GovUkBannerHeadingTagHelper: TagHelper
+{
+    public const string TagName = "govuk-banner-heading";
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "h3";
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.AddClass("govuk-notification-banner__heading", HtmlEncoder.Default);
+        output.Content.AppendHtml(await output.GetChildContentAsync());
+    }
+}
