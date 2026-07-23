cc683173674b15c779c86505bea5df63c67e2dad
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-07-23 11:34:42 +0100

    Upgrade OpenAPI & add OpenTelemetry/Azure Monitor
    - Upgraded Microsoft.OpenApi to 3.9.0 in MockServer and added to AcceptanceTests.
    - Added OpenTelemetry and Azure Monitor NuGet packages to Web project.
    - Introduced AddOpenTelemetryExtensions for telemetry and App Insights integration.
    - Simplified config binding in AddServiceRegistrationExtensions.
    - Updated AzureKeyVaultSecretHealthCheck to remove DefaultAzureCredential.
    - Refactored Program.cs: switched to AddControllersWithViews, improved antiforgery and session config, registered OpenTelemetry, reordered middleware, and cleaned up redundant registrations.

--- src/SFA.DAS.FAA.Web/Program.cs
+++ src/SFA.DAS.FAA.Web/Program.cs
@@ -15,15 +15,22 @@ builder.Services
     .AddOptions()
     .AddMemoryCache()
     .AddValidatorsFromAssembly(typeof(Program).Assembly)
-    .AddControllers(options =>
+    .AddControllersWithViews(options =>
     {
         options.ModelBinderProviders.Insert(0, new MonthYearDateModelBinderProvider());
         options.ModelBinderProviders.Insert(0, new DayMonthYearDateModelBinderProvider());
-    });
+
+        if (!isIntegrationTest)
+        {
+            options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+        }
+        options.Filters.Add(new SignInLinkFilter());
+        options.Filters.Add(new NewFaaUserAccountFilter());
+    })
+    .AddSessionStateTempDataProvider();
 
 builder.Services.AddConfigurationOptions(rootConfiguration);
 
-builder.Services.AddLogging();
 builder.Services.Configure<IISServerOptions>(options => { options.AutomaticAuthentication = false; });
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
@@ -37,8 +44,6 @@ builder.Services.AddHealthChecks()
     .AddCheck<AzureKeyVaultSecretHealthCheck>("KeyVaultSecret", failureStatus: HealthStatus.Unhealthy)
     .AddCheck<SearchIndexStatisticsHealthCheck>("AzureSearchIndex", failureStatus: HealthStatus.Unhealthy);
 
-builder.Services.AddControllersWithViews()
-    .AddSessionStateTempDataProvider();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromSeconds(15);
@@ -46,26 +51,11 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
-builder.Services.Configure<RouteOptions>(_ =>
-{
-
-}).AddMvc(options =>
-{
-    if (!isIntegrationTest)
-    {
-        options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-    }
-
-    options.Filters.Add(new SignInLinkFilter());
-
-    options.Filters.Add(new NewFaaUserAccountFilter());
-});
-
-builder.Services.AddTransient<IStartupFilter,
-    RequestSetOptionsStartupFilter>();
+builder.Services.AddTransient<IStartupFilter, RequestSetOptionsStartupFilter>();
 
 builder.Services.AddDataProtection(rootConfiguration);
 builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddOpenTelemetryRegistration(rootConfiguration["APPLICATIONINSIGHTS_CONNECTION_STRING"]!);
 
 builder.Services.AddExceptionHandler<ResourceNotFoundExceptionHandler>();
 
@@ -80,24 +70,17 @@ else
     app.UseExceptionHandler("/error/500");
 }
 
-
 app.UseHealthChecks();
-
-app.UseAuthentication();
+app.UseCookiePolicy();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.AddRedirectRules();
 app.UseStaticFiles();
-app.UseCookiePolicy();
-
 app.UseSession();
 
-app.UseEndpoints(endpointBuilder =>
-{
-    endpointBuilder?.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=SearchApprenticeshipsController}/{action=Index}/{id?}");
-});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=SearchApprenticeshipsController}/{action=Index}/{id?}");
 
 app.Run();
